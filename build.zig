a329068a762896e1f38b49a978ee8251c55e185f
Pan-Maciek <kozieja.maciej@gmail.com>
2024-02-17 22:57:28 +0100

    add readline support

--- build.zig
+++ build.zig
@@ -24,6 +24,12 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.addIncludePath(.{ .path = "lib" });
+    exe.addLibraryPath(.{ .path = "lib/readline" });
+    exe.linkLibC();
+    exe.linkSystemLibrary("readline");
+    exe.linkSystemLibrary("ncurses");
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
