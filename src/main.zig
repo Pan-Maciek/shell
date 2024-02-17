cb51ee769f4f67bf32b474731389655cb0bdfad1
Pan-Maciek <kozieja.maciej@gmail.com>
2024-02-17 23:45:22 +0100

    os basics

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,28 @@
 const std = @import("std");
+const readline = @import("readline.zig");
 
-const c = @cImport({
-    @cInclude("stdio.h");
-    @cInclude("readline/readline.h");
-});
+const PathIterator = std.mem.SplitIterator(u8, .scalar);
 
 pub fn main() !void {
     const stdout = std.io.getStdOut();
 
     try stdout.writeAll("bytes: []const u8\n");
 
-    var data: [*c]u8 = 0;
-    const out = c.readline(data);
-    defer std.c.free(data);
+    const out = readline.readline("> ");
+    defer std.c.free(out);
 
-    std.debug.print("You entered: {s}", .{out});
+    var buf: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const pwd = try std.os.getcwd(&buf);
+
+    const path = std.os.getenv("PATH") orelse "/bin";
+
+    var iter = PathIterator{
+        .buffer = path,
+        .delimiter = ':',
+        .index = 0,
+    };
+
+    const el = iter.first();
+
+    std.debug.print("{s} {?s}", .{ pwd, el });
 }
